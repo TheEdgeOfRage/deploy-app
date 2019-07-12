bf56fb916b55b5fe81b17d9b842af941a3b839a2
Pavle Portic <pavle.portic@tilda.center>
2019-07-12 15:46:40 +0200

    Change copyright notices

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 /*
  * auth.controller.js
- * Copyright (C) 2019 pavle <pavle.portic@tilda.center>
+ * Copyright (C) 2019 Vivify Ideas
  *
  * Distributed under terms of the BSD-3-Clause license.
  */
