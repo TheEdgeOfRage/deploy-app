068faba910f0f3dc9c5f0f9efb4a26519ba3d383
Pavle Portic <pavle.portic@tilda.center>
2019-07-25 14:55:29 +0200

    Fix url reference in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,16 @@
 
 function deploy() {
 	echo "Starting deploy"
-	URL=$1
-	SERVICES=$2
-	output=$(curl -s -X POST "https://${url}/api/deploy" -H "Content-Type: application/json" -d ${SERVICES})
+	url=$1
+	services=$2
+	output=$(curl -s -X POST "https://${url}/api/deploy" -H "Content-Type: application/json" -d ${services})
 	task_id=$(echo $output | grep "task_id" | cut -d '"' -f 8)
+	echo "Task id: $task_id"
 
 	while true; do
 		output=$(curl -s -X GET "https://${url}/api/tasks/${task_id}" -H "Accept: application/json")
 		echo $output
-		if ! echo $output | grep "Task is still running" >/dev/null; then
+		if ! grep "Task is still running" <<< $output >/dev/null; then
 			break
 		fi
 		echo "Waiting for deploy to finish"
